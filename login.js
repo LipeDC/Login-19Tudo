1e5b7c8920e97933e539aeab12aa5df5e469c5fc
LipeDC <114620346+LipeDC@users.noreply.github.com>
2023-04-14 23:08:10 -0300

    Add files via upload

---
+++ login.js
@@ -0,0 +1,5 @@
+
+const btnEntrar = document.querySelector('.btn');
+btnEntrar.addEventListener('click', function () {
+    window.location.href = "file:///C:/Users/win/Desktop/DepoisLogin/home.html";
+});
